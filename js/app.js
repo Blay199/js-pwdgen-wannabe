1511f9006bbd6419d1ae00a0645c68b52e58a481
Blay199 <a.sorrentino03@gmail.com>
2022-07-19 15:41:02 +0200

    Prompt Done

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const title = document.getElementById('passwordGenerator')
+
+let name = prompt('dimmi come ti chiami')
+let surname = prompt('dimmi il tuo cognome')
+let preferite = prompt ('dimmi il tuo colore preferito')
