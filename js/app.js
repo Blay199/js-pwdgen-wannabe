d7d1273c47f5b83a58ed2f1620f3def0d23e2eb9
Blay199 <a.sorrentino03@gmail.com>
2022-07-19 16:24:37 +0200

    99% Done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 const title = document.getElementById('passwordGenerator')
-
 const name = prompt('dimmi come ti chiami')
 const surname = prompt('dimmi il tuo cognome')
 const preferite = prompt ('dimmi il tuo colore preferito')
 const generetedPwd = name + surname + preferite
 
+title.innerHTML = 'La tua password è' + generetedPwd
+
+// console.log(name);
+// console.log(surname);
+// console.log(preferite);
 console.log(generetedPwd)
+
+
